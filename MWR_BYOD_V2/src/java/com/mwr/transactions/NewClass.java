80dd02052167782ebeed8177ed74c15e7e8d311c
Heindrich Paul <heindrich.paul@gmail.com>
2013-08-29 19:06:12 +0200

    Madene se foldertjie

---
+++ MWR_BYOD_V2/src/java/com/mwr/transactions/NewClass.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mwr.transactions;
+
+/**
+ *
+ * @author Heindrich
+ */
+public class NewClass {
+}
