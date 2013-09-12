1d73c19df3900e5610a762df678dd00ca7ee0789
Steyna <Steyna@Steyna-PC>
2013-09-12 19:26:31 +0200

    removed unnecessary lib import

--- MWR_BYOD_V2/src/java/com/mwr/controller/ControllerServlet.java
+++ MWR_BYOD_V2/src/java/com/mwr/controller/ControllerServlet.java
@@ -16,7 +16,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
