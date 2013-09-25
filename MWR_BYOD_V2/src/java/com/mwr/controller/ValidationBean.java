d18b5f67743f5ee3ed75f7dd172673a26cd1bda1
Steyna <Steyna@Steyna-PC.dummy.porta.siemens.net>
2013-09-25 10:05:01 +0200

    added quick register functionality

--- MWR_BYOD_V2/src/java/com/mwr/controller/ValidationBean.java
+++ MWR_BYOD_V2/src/java/com/mwr/controller/ValidationBean.java
@@ -1,4 +1,3 @@
-
 package com.mwr.controller;
 
 import com.mwr.database.Device;
@@ -14,24 +13,107 @@ import org.hibernate.classic.Session;
 @SessionScoped
 public class ValidationBean {
 
-        private Boolean exist = null;
-        private DatabaseJSFManagedBean d;
-        
-        public Boolean empExist(String id)
-        {
-            Employee e = d.getEmployee(id);
-            if (e == null)
-                exist = false;
-            else exist = true;
-            return exist;
-        }
-        public Boolean devExist(int id)
-        {
-            Device e = d.getDevice();
-            if (e == null)
-                exist = false;
-            else exist = true;
-            return exist;
-        }
+    private Boolean devExist = false;
+    private Boolean empExist = false;
+    private String devExists = "";
+    private String empExists = "";
+    private String token = "";
+    private String empID;
+    private String devID;
+    private Employee emp;
+    private Device empDev;
+    private DatabaseJSFManagedBean d;
 
+    public String empExist() {
+        emp = d.getEmployee(empID);
+        if (emp == null) {
+            empExist = false;
+            empExists = empID + " does not exist!";
+
+        } else {
+            empExist = true;
+            empExists = "Valid!";
+        }
+        return empExists;
+    }
+
+    public String devExist() {
+        empDev = d.getDevice(devID);
+        if (empDev == null) {
+            devExist = false;
+            devExists = devID + " does not exist!";
+
+        } else {
+            devExist = true;
+            devExists = "Valid!";
+        }
+        return devExists;
+    }
+
+    public Boolean getDevExist() {
+        return devExist;
+    }
+
+    public void setDevExist(Boolean devExist) {
+        this.devExist = devExist;
+    }
+
+    public Boolean getEmpExist() {
+        return empExist;
+    }
+
+    public void setEmpExist(Boolean empExist) {
+        this.empExist = empExist;
+    }
+
+    public String getDevExists() {
+        return devExists;
+    }
+
+    public void setDevExists(String devExists) {
+        this.devExists = devExists;
+    }
+
+    public String getEmpExists() {
+        return empExists;
+    }
+
+    public void setEmpExists(String empExists) {
+        this.empExists = empExists;
+    }
+
+    public String getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(String empID) {
+        this.empID = empID;
+    }
+
+    public String getDevID() {
+        return devID;
+    }
+
+    public void setDevID(String devID) {
+        this.devID = devID;
+    }
+
+    public String getToken() {
+        return token;
+    }
+    
+    public String getDevToken()
+    {
+        if (empExist == false)
+            return empExists;
+        if (devExist == false)
+            return devExists;
+        
+        return empDev.getToken();
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+    
 }
